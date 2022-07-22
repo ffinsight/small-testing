d095c73cfc99fd4f3b0b256b57dc3d28b311c42d
ffinsight <franc.feudjio@insight.com>
2022-07-22 12:44:50 -0500

    repo1

--- dir1/main.tf
+++ dir1/main.tf
@@ -2,3 +2,69 @@ resource "azurerm_resource_group" "test" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+tags = {
+  environment = "dev"
+}
+
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet-1" {
+  name                 = var.pub-subnet_name
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = var.priv-subnet_name
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  tags = {
+    environment = "dev"
+  }
+}
--- dir1/variable.tf
+++ dir1/variable.tf
@@ -7,3 +7,28 @@ variable "resource_group_location" {
   type        = string
   description = "RG location in Azure"
 }
+
+variable "virtual_network_name" {
+  type        = string
+  description = "VNET name in Azure"
+}
+
+variable "pub-subnet_name" {
+  type        = string
+  description = "pub-Subnet name in Azure"
+}
+
+variable "priv-subnet_name" {
+  type        = string
+  description = "priv-Subnet name in Azure"
+}
+
+variable "public_ip_name" {
+  type        = string
+  description = "Public IP name in Azure"
+}
+
+variable "network_security_group_name" {
+  type        = string
+  description = "NSG name in Azure"
+}
